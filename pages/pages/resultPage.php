f28d3c8fe31f2c7c844980211af5c7731e2abc7e
Thorr Stevens <thorr.stevens@student.howest.be>
2015-11-21 00:03:21 +0100

    Last Edit to ResultPage due to time shortage

--- pages/pages/resultPage.php
+++ pages/pages/resultPage.php
@@ -9,7 +9,7 @@
                     <h2>MAYAN SUN–SOUTHBOUND</h2>
                     <p class="details">16 days, Playa del Carmen to Antigua</p>
 
-                    
+                    <img src="img/16DayMayanSunYolo.png" style="position: absolute; width: 300px; height: 300px; left: 50%; margin-left: -150px;" alt="16DayMayanSunYolo" />
                     
                     <section class="submitSection">
                         <header><h1>View Details</h1></header>
